d973d1f2ce24db81d6a2773bff3c510ac93a5cda
Timothy Bailey <mail@dispossible.com>
2023-06-25 12:57:14 +0100

    Added Favicons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,16 @@ export default function Home({ projects, photos }: InferGetStaticPropsType<typeo
                 <title>Dispossible</title>
                 <meta name="description" content="Dispossible - Timothy Bailey - Portfolio" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+
+                <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg" />
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#232620" />
+                <meta name="msapplication-TileColor" content="#232620" />
+                <meta name="theme-color" content="#232620" />
             </Head>
             <main>
                 <Banner />
