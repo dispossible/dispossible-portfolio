8f033b87b99892c472a8ab841a1943f4b15c36b2
Timothy Bailey <mail@dispossible.com>
2023-07-06 00:06:44 +0100

    Some style and component fixes

--- src/components/ProjectPage/index.tsx
+++ src/components/ProjectPage/index.tsx
@@ -1,4 +1,5 @@
 import { ProjectPostMeta } from "@/lib/projects";
+import Link from "next/link";
 import { CSSProperties } from "react";
 import styles from "./projectPage.module.css";
 
@@ -13,6 +14,9 @@ export default function ProjectPage({ title, date, thumb, children }: ProjectPag
                 <h1 className={styles.title}>{title}</h1>
             </header>
             <div className={styles.content}>{children}</div>
+            <footer className={styles.footer}>
+                <Link href="/#projects">&lt; Return to project list</Link>
+            </footer>
         </article>
     );
 }
--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import style from "./video.module.css";
 
 interface VideoProps {
@@ -7,7 +8,17 @@ interface VideoProps {
 
 export default function Video({ youtubeId, title }: VideoProps) {
     const url = `https://www.youtube.com/embed/${youtubeId}?color=white&amp;modestbranding=1&amp;playsinline=1&amp;rel=0`;
-    const thumbUrl = `https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`;
+    const [thumbUrl, setThumbUrl] = useState(`https://img.youtube.com/vi/${youtubeId}/maxresdefault.jpg`);
+
+    useEffect(() => {
+        const img = window.document.createElement("img");
+        img.addEventListener("load", (e) => {
+            if (img.width < 150) {
+                setThumbUrl(`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`);
+            }
+        });
+        img.src = thumbUrl;
+    }, [youtubeId]);
 
     const thumbnail = `<style>*{padding:0;margin:0;overflow:hidden}body,html{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto;height:100%;object-fit:cover}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:#fff;text-shadow:0 0 .5em #000}</style><a href="${url}&amp;autoplay=1"><img src="${thumbUrl}" alt="${title}" /><span>&#x25BA;</span></a>`;
 
