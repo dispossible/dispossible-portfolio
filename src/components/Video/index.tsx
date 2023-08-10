0cb32448f3246befd31a2e8e23fe3f59c77ad378
Timothy Bailey <mail@dispossible.com>
2023-08-10 19:01:45 +0100

    Fixed warning

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -18,6 +18,7 @@ export default function Video({ youtubeId, title }: VideoProps) {
             }
         });
         img.src = thumbUrl;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [youtubeId]);
 
     const thumbnail = `<style>*{padding:0;margin:0;overflow:hidden}body,html{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto;height:100%;object-fit:cover}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:#fff;text-shadow:0 0 .5em #000}</style><a href="${url}&amp;autoplay=1"><img src="${thumbUrl}" alt="${title}" /><span>&#x25BA;</span></a>`;
